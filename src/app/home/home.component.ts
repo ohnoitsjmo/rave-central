48d75ff9bbd74718aaf5d4d07fe8fd8aa91568d2
Justin Mo <jmo02@calpoly.edu>
2018-07-29 12:04:14 -0700

    member search frontend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MemberObject } from '../memberobject'
-import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,21 +7,9 @@ import {HttpClient} from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  //Data
-  allMembers:any[];
-  name="";
-  num="";
-  phone="";
-  date="";
-
-  constructor(private http:HttpClient) { }
+  constructor() { }
 
   ngOnInit() {
-    this.allMembers = [new MemberObject("Justin", "12345678", "4089921767", Date()), 
-    new MemberObject("Dominic", "876543231", "5101231234", Date()),
-    new MemberObject("Patrick", "151235552", "6262348812", Date())]
-    // this.http.post('/getAllMembers', {}).subscribe(res => {
-    //   this.allMembers = res as MemberObject[];
-    // })
   }
+
 }
